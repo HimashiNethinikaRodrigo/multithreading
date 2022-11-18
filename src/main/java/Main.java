d8281e768b07d7a2c6e28404cfefbdf9ce7d6607
himashi_rodrigo <himashirodrigo23@gmail.com>
2022-11-18 14:54:27 +0100

    create basic thread and start it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Thread thread = new Thread();
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                // code that will run in a new thread
+            }
+        });
+
+        // start thread - jvm to create new thread and pass it to operating system
+        thread.start();
     }
 }
