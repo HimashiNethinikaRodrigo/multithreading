91f5891e6bfca7f6bc0bef5c7e35754eae7f62f5
Himashi Rodrigo <himashirodrigo23@gmail.com>
2022-11-21 19:10:29 +0100

    Create threads with class (#4)
    * create thread by extending class from java Thread class
    
    * create thread by extending class from java Thread class

--- src/main/java/com/edu/thread/threadclass/ThreadClass_1.java
+++ src/main/java/com/edu/thread/threadclass/ThreadClass_1.java
@@ -1,8 +1,22 @@
 package com.edu.thread.threadclass;
 
-public class ThreadClass_1 extends Thread{
-    @Override
-    public void run() {
-
+public class ThreadClass_1{
+    public static void main(String[] args) {
+        Thread thread = new WorkerThread();
+        thread.setName("Worker Thread");
+        thread.start();
     }
+
+    private static class WorkerThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello from " + this.getName());
+        }
+    }
+
+
+    /*
+     * Output:
+     * Hello from Worker Thread
+     */
 }
